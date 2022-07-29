c0205fc67369f30e731292435158534827e33f45
vaishnavi shukla <vaishnavishukla9074@gmail.com>
2022-07-29 14:58:18 +0530

    implemented card functionality in service layer

--- src/com/group9/service/MetroSystemService.java
+++ src/com/group9/service/MetroSystemService.java
@@ -1,5 +1,10 @@
 package com.group9.service;
 
+import com.group9.bean.MetroCard;
+
 public interface MetroSystemService {
+	
+	boolean addCard(MetroCard card);
+	double cardBalance(long cardId);
 
 }
