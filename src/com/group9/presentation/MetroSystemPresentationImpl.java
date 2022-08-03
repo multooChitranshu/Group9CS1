39ae397c8b857e7706cff211cbff8191c6608a76
MohitKhandelwal06 <khandelwalmohit006@gmail.com>
2022-08-03 20:17:28 +0530

    :zap:Bug Fixed! constant log out problem solved

--- src/com/group9/presentation/MetroSystemPresentationImpl.java
+++ src/com/group9/presentation/MetroSystemPresentationImpl.java
@@ -15,6 +15,98 @@ public class MetroSystemPresentationImpl implements MetroSystemPresentation {
 		System.out.println("0. Exit");
 	}
 	
+	public void existingUserMenu() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter your card Id : ");
+		long id=scanner.nextLong();
+		int val=0;
+		//checking valid user
+		while(true) {
+		if(metroServiceImpl.checkCard(id)) {
+			//calling transaction function
+			Transaction t=metroServiceImpl.lastTransaction(id);
+			//if entry closed (swipe-in)(val=1)
+			if( t==null || (t.getDateAndTimeOfExit()!=null))
+			{
+				System.out.println("1. Swipe-In");
+				val=1;
+			}
+			//if entry open (swipe-out)(val=2)
+			else if(t.getDateAndTimeOfBoarding() !=null && t.getDateAndTimeOfExit()== null)
+			{
+				System.out.println("1. Swipe-Out");
+				val=2;
+			}
+			//check balance
+			System.out.println("2. Check Balance");
+			//recharge card
+			System.out.println("3. Recharge Card");
+			//transaction table
+			System.out.println("4. Get all previous transactions");
+			//exit
+			System.out.println("5. Sign out");
+//			sign out to perform menu
+			System.out.println("Enter your choice: ");
+			int ch =scanner.nextInt();
+			switch (ch) {
+			case 1:
+				switch (val) {
+				case 1:
+					System.out.println("Enter source station id : ");
+					int ssi=scanner.nextInt();
+					if(metroServiceImpl.swipeIn(id,ssi))
+						System.out.println("Swipe-In successful");
+					else
+						System.out.println("Swipe-In un-successful");
+					break;
+				case 2:
+					System.out.println("Enter destination station id : ");
+					int dsi=scanner.nextInt();
+					if(metroServiceImpl.swipeOut(id,dsi)) {
+						System.out.println("Swipe-Out successful");
+						System.out.println("Rs. "+metroServiceImpl.lastTransaction(id).getFare()+" was deducted. Thank you!");
+					}
+					else
+						System.out.println("Swipe-Out un-successful");
+					break;
+				}
+				break;
+			case 2:
+				double bal=metroServiceImpl.cardBalance(id);
+				System.out.println("Available balance : "+bal);
+				break;
+			case 3:
+				System.out.println("Enter the amount : ");
+				double amt=scanner.nextDouble();
+				if(amt<0)
+					System.out.println("Enter valid amount please...");
+				else
+				metroServiceImpl.rechargeCard(id,amt);
+				amt=metroServiceImpl.cardBalance(id);
+				System.out.println("Available balance : "+amt);
+				break;
+			case 4:
+				//incomplete
+				List<Transaction> history=metroServiceImpl.transactionHistory(id);
+				for(int i=0;i<history.size();i++) {
+					System.out.println(history.get(i));
+				}
+				break;
+			case 5:
+				return;
+			case 0:
+				System.out.println("Thanks for using Metro System");
+				System.exit(0);
+			default :
+				System.out.println("Invalid Choice");
+			}
+		}
+		else {
+			System.out.println("Card does not exists please check the card number again");
+		}
+		}
+	}
+	
 	@Override
 	public void performMenu(char choice) {
 		Scanner scanner=new Scanner(System.in);
@@ -31,126 +123,54 @@ public class MetroSystemPresentationImpl implements MetroSystemPresentation {
 				System.out.println("Welcome to our Metro System.\nYour card added successfully");
 				System.out.println("Your card details are: "+metroCard);
 				//swipe-in and check balance
-				int internal_choice;
-				System.out.println("1. swipe-in");
-				System.out.println("2. check balance");
-				System.out.println("3. Recharge Card");
-				internal_choice=scanner.nextInt();
-				switch (internal_choice) {
-				case 1:
-					//swipe in
-					System.out.println("Enter source station id : ");
-					int ssi=scanner.nextInt();
-					if(metroServiceImpl.swipeIn(aadhar,ssi))
-						System.out.println("Swipe-In successful");
-					else
-						System.out.println("Swipe-In un-successful");
+				System.out.println("Press 1 to continue to booking");
+				System.out.println("Press any other number to sign out");
+				int temp_choice=scanner.nextInt();
+				if(temp_choice==1) {
+				existingUserMenu();
+				}else{
 					break;
-				case 2:
-					//check balance
-					System.out.println("Available balance : "+metroServiceImpl.cardBalance(aadhar));
-				
-				case 3:
-					System.out.println("Enter the amount : ");
-					double amt=scanner.nextDouble();
-					if(amt<0)
-						System.out.println("Enter valid amount please...");
-					else
-					metroServiceImpl.rechargeCard(aadhar,amt);
-					amt=metroServiceImpl.cardBalance(aadhar);
-					System.out.println("Available balance : "+amt);
-				break;
-				default:
-					System.out.println("Enter a valid option");
-			}
+				}
+//				int internal_choice;
+//				System.out.println("1. swipe-in");
+//				System.out.println("2. check balance");
+//				System.out.println("3. Recharge Card");
+//				
+//				internal_choice=scanner.nextInt();
+//				switch (internal_choice) {
+//				case 1:
+//					//swipe in
+//					System.out.println("Enter source station id : ");
+//					int ssi=scanner.nextInt();
+//					if(metroServiceImpl.swipeIn(aadhar,ssi))
+//						System.out.println("Swipe-In successful");
+//					else
+//						System.out.println("Swipe-In un-successful");
+//					break;
+//				case 2:
+//					//check balance
+//					System.out.println("Available balance : "+metroServiceImpl.cardBalance(aadhar));
+//				
+//				case 3:
+//					System.out.println("Enter the amount : ");
+//					double amt=scanner.nextDouble();
+//					if(amt<0)
+//						System.out.println("Enter valid amount please...");
+//					else
+//					metroServiceImpl.rechargeCard(aadhar,amt);
+//					amt=metroServiceImpl.cardBalance(aadhar);
+//					System.out.println("Available balance : "+amt);
+//				break;
+//				default:
+//					System.out.println("Enter a valid option");
+//			}
 			}
 			else {
 				System.out.println("Failed to add the card. Card already exists!");
 			}
 			break;
 		case 'N':
-			System.out.println("Enter your card Id : ");
-			id=scanner.nextLong();
-			int val=0;
-			//checking valid user
-			if(metroServiceImpl.checkCard(id)) {
-				//calling transaction function
-				Transaction t=metroServiceImpl.lastTransaction(id);
-				//if entry closed (swipe-in)(val=1)
-				if( t==null || (t.getDateAndTimeOfExit()!=null))
-				{
-					System.out.println("1. Swipe-In");
-					val=1;
-				}
-				//if entry open (swipe-out)(val=2)
-				else if(t.getDateAndTimeOfBoarding() !=null && t.getDateAndTimeOfExit()== null)
-				{
-					System.out.println("1. Swipe-Out");
-					val=2;
-				}
-				//check balance
-				System.out.println("2. Check Balance");
-				//recharge card
-				System.out.println("3. Recharge Card");
-				//transaction table
-				System.out.println("4. Get all previous transactions");
-				//exit
-				System.out.println("Enter your choice: ");
-				int ch =scanner.nextInt();
-				switch (ch) {
-				case 1:
-					switch (val) {
-					case 1:
-						System.out.println("Enter source station id : ");
-						int ssi=scanner.nextInt();
-						if(metroServiceImpl.swipeIn(id,ssi))
-							System.out.println("Swipe-In successful");
-						else
-							System.out.println("Swipe-In un-successful");
-						break;
-					case 2:
-						System.out.println("Enter destination station id : ");
-						int dsi=scanner.nextInt();
-						if(metroServiceImpl.swipeOut(id,dsi)) {
-							System.out.println("Swipe-Out successful");
-							System.out.println("Rs. "+metroServiceImpl.lastTransaction(id).getFare()+" was deducted. Thank you!");
-						}
-						else
-							System.out.println("Swipe-Out un-successful");
-						break;
-					}
-					break;
-				case 2:
-					double bal=metroServiceImpl.cardBalance(id);
-					System.out.println("Available balance : "+bal);
-					break;
-				case 3:
-					System.out.println("Enter the amount : ");
-					double amt=scanner.nextDouble();
-					if(amt<0)
-						System.out.println("Enter valid amount please...");
-					else
-					metroServiceImpl.rechargeCard(id,amt);
-					amt=metroServiceImpl.cardBalance(id);
-					System.out.println("Available balance : "+amt);
-					break;
-				case 4:
-					//incomplete
-					List<Transaction> history=metroServiceImpl.transactionHistory(id);
-					for(int i=0;i<history.size();i++) {
-						System.out.println(history.get(i));
-					}
-					break;
-				case 0:
-					System.out.println("Thanks for using Metro System");
-					System.exit(0);
-				default :
-					System.out.println("Invalid Choice");
-				}
-			}
-			else {
-				System.out.println("Card does not exists please check the card number again");
-			}
+			existingUserMenu();
 			break;
 		case '0':
 			System.out.println("Thanks for using Metro System");
